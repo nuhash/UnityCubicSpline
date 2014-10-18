4aed1693d39a72aab1e0066d463080784811793e
NuHash <sameh.hussain@nuhash.net>
2014-10-18 14:05:06 +0100

    Added example usage

--- CubicSpline.cs
+++ CubicSpline.cs
@@ -7,10 +7,15 @@ public class CubicSpline
 	readonly object _queueLock = new object();
 	int queue = 0;
 	float length =0;
-	int iterations = 50;
+	public int iterations = 50; //Number of iterations for numerical solver when calculating the length of the spline
 	float h;
 	Vector3[] coefficients = new Vector3[4];
 
+	// CubicSpline() - Calculates coefficients for cubic spline equation
+	// position0 - Starting position
+	// gradient0 - Starting gradient
+	// position1 - Ending position
+	// gradient1 - Ending gradient
 	public CubicSpline (Vector3 position0, Vector3 gradient0, Vector3 position1, Vector3 gradient1)
 	{
 		coefficients[3] = 2*position0 + gradient0 - 2*position1 + gradient1;
@@ -20,6 +25,9 @@ public class CubicSpline
 		h =  1/Convert.ToSingle(iterations);
 	}
 
+	//GetSamples() - Returns a List<Vector3> of positions along the spline which are even distributed in t-space (points are not evenly distributed across the spline)
+	//numSamples - The number of points to sample along the spline
+	//TODO - Implement even distribution of points (much more expensive)
 	public List<Vector3> GetSamples(int numSamples)
 	{
 		List<Vector3> samples = new List<Vector3>();
@@ -31,8 +39,12 @@ public class CubicSpline
 		return samples;
 	}
 
+	//Sample() - Returns a sample of the spline using a t-space reference
+	//t - Range: [0.0,1.0] - 0.0 returns the starting position, 1.0 returns the ending position, 0.5 returns the position approximately halfway along the spline
 	public Vector3 Sample(float t)
 	{
+		if(t>1 || t<0)
+			throw new ArgumentException("Variable out of range. Must be in range [0.0,1.0]", "t");
 		return coefficients[3]*t*t*t+coefficients[2]*t*t+coefficients[1]*t+coefficients[0];
 	}
 
--- README.md
+++ README.md
@@ -1,4 +1,26 @@
 UnityCubicSpline
 ================
 
-Calculates cubic hermite splines, coded to work in Unity using C#
+Calculates cubic hermite splines, coded to work in Unity using C#.
+
+The functionality is 100% finished, there are a few features I intend to add in the future but they aren't necessary for me right now (possibly not ever). The numerical solver for calculating the length is RK4 (Fourth Order Runga-Kutta), it can easily by modified to RKF45 (Runga-Kutta Fehlberg, Fourth order with Fifth order error estimator), however, the benefits of doing so will be minimal.
+
+I have provided an example below on how to use the script, the functions are pretty straight-forward to use so it shouldn't take long for you to figure out how to use it.
+
+The licence is MIT.
+
+If there are any bugs please let me know.
+
+Usage
+-----
+```C#
+CubicSpline splineSampler = new CubicSpline(Vector3.zero, Vector3.right, Vector3.forward,-Vector3.right);
+float splineLength = splineSampler.Length();
+int numSamples = Mathf.CeilToInt(splineLength/0.5f);
+List<Vector3> splineSamples = splineSampler.GetSamples(numSamples);
+for(int i = 0;i<splinesSamples.Count;i++)
+{
+	var cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+	cube.transform.position = splineSamples[i];
+}
+```
